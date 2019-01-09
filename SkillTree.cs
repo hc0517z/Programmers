fa0ecc8af6cc88eef065e0b208c5cfc5d981c72a
olapapa <hc0517z@gmail.com>
2019-01-09 17:27:33 +0900

    commit

--- SkillTree.cs
+++ SkillTree.cs
@@ -8,6 +8,21 @@ namespace Programmers
     {
         public int GetSkillTreeCount(string skill, string[] skill_trees) {
             int answer = 0;
+
+            foreach (string skillTree in skill_trees)
+            {
+                var newSkillTree =  skillTree.Select(c => skill.IndexOf(c)).Where(i => i != -1).ToArray();
+                answer++;
+                for (int i = 0; i < newSkillTree.Length-1; i++)
+                {
+                    if (newSkillTree[i] > newSkillTree[i + 1])
+                    {
+                        answer--;
+                        break;
+                    }
+                }
+            }
+            
             return answer;
         }
     }
