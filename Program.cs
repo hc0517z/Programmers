e7cc3c6f307243b68717c594e75fc081f706829e
olapapa <hc0517z@gmail.com>
2019-01-09 16:35:59 +0900

    commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Programmers
+﻿using System;
+
+namespace Programmers
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            SkillTree st = new SkillTree();
+            var count = st.GetSkillTreeCount("CBD", new string[] {"BACDE", "CBADF", "AECB", "BDA"});
+            Console.WriteLine(count);
         }
     }
 }
---
+++ SkillTree.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+namespace Programmers
+{
+    //    CBD	[BACDE, CBADF, AECB, BDA]	2
+    public class SkillTree
+    {
+        public int GetSkillTreeCount(string skill, string[] skill_trees) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
